0960a4b1fb407659c7d5fa39badfe8b8442aca5d
Elijah Cox <elijah.thomas.cox@gmail.com>
2023-11-10 10:38:07 -0600

    rm comments

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -27,18 +27,12 @@ initGrid();
 
 function initGrid(){
     for(var i = 0; i < rows; i++){
-        //create row div
         const rowDiv = document.createElement('div');
         for(var j = 0; j < cols; j++){
-            //create div
             const squareDiv = document.createElement('div');  
             squareDiv.setAttribute('style', 'height: 40px; width: 40px; background-color: lightgrey;border-style: dotted;border-color: lightgrey;display: inline-block;');  
-            //squareDiv.onmouseover = function(){squareDiv.style.backgroundColor="lightgreen";}
-            //add css styling to make it a grid, give a grey colors
-            //add to row div
             rowDiv.appendChild(squareDiv);
         }
         containerDiv.appendChild(rowDiv);
-        //add row div to containerDiv
     }
 } 
