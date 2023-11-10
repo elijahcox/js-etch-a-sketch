ea83369cdae889246e3b6a91b1ac17f8452612f6
Elijah Cox <elijah.thomas.cox@gmail.com>
2023-11-09 21:11:35 -0600

    fill in initgrid() function

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,8 +1,17 @@
 var rows = 16;
 var cols = 16;
 const containerDiv = document.querySelector('.etch-a-sketch-container');
-containerDiv.innerHTML = "he";
+
+initGrid();
 
 function initGrid(){
-
+    for(var i = 0; i < rows; i++){
+        //create row div
+        for(var j = 0; j < cols; j++){
+            //create div
+            //add css styling to make it a grid, give a grey colors
+            //add to row div
+        }
+        //add row div to containerDiv
+    }
 } 
